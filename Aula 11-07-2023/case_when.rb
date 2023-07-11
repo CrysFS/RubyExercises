0d516a3fc7720eb476fb03257b5a809edd36b1a1
CrysFs <crystianschlemper@gmail.com>
2023-07-11 19:43:44 -0300

    adicionando case when e conditional atri..

---
+++ Aula 11-07-2023/case_when.rb
@@ -0,0 +1,10 @@
+case expressão
+when valor1
+    >>executado se expressão for igual a valor1
+when valor2
+    >>executado se expressão for igual a valor2
+when valor3
+    >>executado se expressão for igual a valor3
+else
+    >>executado se nenhum dos casos anteriores for correspondido
+end
---
+++ Aula 11-07-2023/conditional_atribuition.rb
@@ -0,0 +1,9 @@
+name = nil
+name ||= 'John Doe'
+puts name
+
+name ||='João'
+puts name
+
+#o operador ||= verifica se ja há algo atribuido à variavel e caso sim ele não atribui novamente
+
