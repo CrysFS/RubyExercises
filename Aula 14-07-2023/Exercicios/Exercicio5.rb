ff7e2659b9b3b9bb5eea32415f172d1098e82f83
CrysFs <crystianschlemper@gmail.com>
2023-07-14 20:22:18 -0300

    adicionado arquivo de aprendizado

---
+++ Aula 14-07-2023/Exercicios/Exercicio5.rb
@@ -0,0 +1,9 @@
+# 5- Crie uma classe chamada "ContaBancaria" com os atributos "saldo" e "proprietario".
+# O saldo deve ser iniciado com valor igual a zero. Implemente um método para depositar
+# dinheiro na conta.
+
+class conta_bancaria
+
+    def saldo 
+
+        
