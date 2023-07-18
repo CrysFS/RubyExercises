10baa332952d9a58d2bbdb207c48fdcaec5f783d
CrysFs <crystianschlemper@gmail.com>
2023-07-17 21:28:24 -0300

    completing

--- RUBY 2/Aula 17-07-2023/Exercicio/sandbox.rb
+++ RUBY 2/Aula 17-07-2023/Exercicio/sandbox.rb
@@ -2,27 +2,33 @@ require './animal'
 require './dog'
 require './cat'
 require './mamiferos'
-require './aves'
+require './birds'
 
-# def display_animals(animal1, animal2)
-#     animal1.speak
-#     animal2.speak
-# end
+def display_animals(animal1, animal2, animal3)
+    animal1.speak
+    animal2.speak
+      animal3.fly  
+end
 
-# catioro = Dog.new(
-#     name: 'Rex'
-# )
-
-# gatito = Cat.new(
-#     name: 'GatoLazarento'
-# )
-
-# display_animals(catioro, gatito)
-
-cachioro = Dog.new(
-    name: 'Rino'
+catioro = Dog.new(
+    name: 'Rex'
 )
 
-cachioro.speak
+gatito = Cat.new(
+    name: 'GatoLazarento'
+)
+
+lico = Birds.new(
+    name: 'lorem ipsum'
+)
+
+display_animals(catioro, gatito, lico)
+
+# cachioro = Dog.new(
+#     name: 'Rino'
+# )
+
+# cachioro.speak
+
 
 
