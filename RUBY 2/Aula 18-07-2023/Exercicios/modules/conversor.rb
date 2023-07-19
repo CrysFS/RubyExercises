83a338c3764649ed152ce77be13e23b425e39088
CrysFs <crystianschlemper@gmail.com>
2023-07-18 21:29:38 -0300

    changins sequence

--- RUBY 2/Aula 18-07-2023/Exercicios/modules/conversor.rb
+++ RUBY 2/Aula 18-07-2023/Exercicios/modules/conversor.rb
@@ -1,11 +1,12 @@
 module Conversor
+    INCH = 2.54
     
     def inches_to_centimeters(inches)
         inches * 2.54
     end
     
     def centimeters_to_inches(centimeters)
-        centimeters / 2.54
+        centimeters / INCH
     end
 
 end 
