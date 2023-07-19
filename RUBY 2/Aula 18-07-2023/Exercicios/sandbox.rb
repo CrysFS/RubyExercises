a10d0f47cb34796865092661b8fef5d3531eaf5f
CrysFs <crystianschlemper@gmail.com>
2023-07-18 21:29:57 -0300

    alterning

--- RUBY 2/Aula 18-07-2023/Exercicios/sandbox.rb
+++ RUBY 2/Aula 18-07-2023/Exercicios/sandbox.rb
@@ -10,9 +10,10 @@ calculator = Calculator.new
 # puts calculator.centimeters_to_inches(30) # 11.811023622047244
 # puts calculator.sum(5, 7) # 12
 # puts calculator.product(3, 4) # 12
-puts calculator.dollar_to_euro(23)
-puts calculator.euro_to_dollar(23)
-puts calculator.real_to_dollar(23)
-puts calculator.dollar_to_real(23)
-puts calculator.euro_to_real(23)
 puts calculator.real_to_euro(23)
+puts calculator.real_to_dollar(23)
+puts calculator.euro_to_real(23)
+puts calculator.euro_to_dollar(23)
+puts calculator.dollar_to_real(23)
+puts calculator.dollar_to_euro(23)
+
